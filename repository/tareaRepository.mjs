c613bfb1721ff64394f12717058ed2bf34f050cf
armandie2013 <arman_die@hotmail.com>
2025-02-19 15:17:10 -0300

    Tarea Repository

--- repository/tareaRepository.mjs
+++ repository/tareaRepository.mjs
@@ -1,22 +1,44 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import tareasDataSource from './tareasDataSource.mjs';
-import tarea from '../models/tarea.mjs';
+import TareasDataSource from './tareasDataSource.mjs';
+import Tarea from '../models/tarea.mjs';
 
-const __filename= fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
-const filePath=path.join(__dirname,'../tareas.txt');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, '../tareas.txt');
 
-export default class tareaRepository extends tareasDataSource{
-    constructor(){
+export default class TareaRepository extends TareasDataSource {
+    constructor() {
         super();
     }
-    obtenerTodas(){
-        try{
-            const data=fs.readFileSync(filePath, 'utf-8');
-            const tareas=JSON.parse(data);
-            return tareas.map(tareaData=>new tarea(tareaData.id, tareaData.titulo, tareaData.descripcion, tareaData.completado));        
+    obtenerTodas() {
+        try {
+            const data = fs.readFileSync(filePath, 'utf-8');
+            const tareas = JSON.parse(data);
+            return tareas.map(tareaData => new Tarea (tareaData.id, tareaData.titulo, tareaData.descripcion, tareaData.completado));
+        } catch (error) {
+            console.error("Error al leer el archivo de tareas:", error);
+            return [];
+        }
+    }
+
+    guardar(tareas) {
+        try {
+            const data = JSON.stringify(tareas, null, 2);
+            fs.writeFileSync(filePath, data, 'utf-8');
+        } catch (error) {
+            console.error("Error al guardar las tareas:", error);
+        }
+    }
+
+    eliminar(id) {
+        try {
+            const tareas = this.obtenerTodas();
+            const tareasActualizadas = tareas.filter(data => data.id !== id);
+            this.guardar(tareasActualizadas);
+        } catch (error) {
+            console.error("Error al eliminar la tarea:", error);
         }
     }
 }
